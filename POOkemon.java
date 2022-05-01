3fe139f54cb28e1729fe64a01b9c98c773368872
psga <pusugag@unal.edu.co>
2022-05-01 20:32:37 +0000

    prueba

---
+++ POOkemon.java
@@ -0,0 +1,10 @@
+public class POOkemon {
+    public static void main(String[] args){
+       String pok="░░░░╔╗\n" +"╔═╦═╣╠╦═╦══╦═╦═╦╗\n" +"║╬║╬║═╣╩╣║║║╬║║║║\n"+"║╔╩═╩╩╩═╩╩╩╩═╩╩═╝\n" +"╚╝\n";
+
+       System.out.println(pok+"\n" +
+               "Vamos a crear tu entrenador\nDigite su nombre:");
+
+
+    }
+}
