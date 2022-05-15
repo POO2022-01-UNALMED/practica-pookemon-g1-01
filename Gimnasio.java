2502444e4be49a1ec066f7ed7b83e9f905baf5d7
shirohigexe <99839298+shirohigexe@users.noreply.github.com>
2022-05-14 21:52:42 -0500

    Create Gimnasio.java

---
+++ Gimnasio.java
@@ -0,0 +1,28 @@
+
+public class Gimnasio {
+	
+	private String Nombre;
+	private EntrenadorGimnasio EntrenadorGym;
+	
+	public Gimnasio(EntrenadorGimnasio EntrenadorGym) {
+		this.Nombre = "Gym Defecto";
+		this.EntrenadorGym = EntrenadorGym;
+		}
+	
+	public String getNombre() {
+		return this.Nombre;
+	}
+	public void setNombre(String nuevoNombre) {
+		this.Nombre = nuevoNombre;
+	}
+	public String getEGym() {
+		return "el entrenador de este gimnasio es: " + this.EntrenadorGym.getNombre();
+	}
+	public void setEGym(EntrenadorGimnasio nuevoEntrenador) {
+		this.EntrenadorGym = nuevoEntrenador;
+	}
+	public String getPOOKEMON() {
+		return "el Pokemon inicial del Entrenador es: " + this.EntrenadorGym.getPokemonInicial();	
+	}	
+}
+
