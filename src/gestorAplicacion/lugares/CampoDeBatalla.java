6a12613300715ae44a96642b7a936a2888d3bbb7
psga <pusugag@unal.edu.co>
2022-06-03 04:03:23 +0000

    main

--- src/gestorAplicacion/lugares/CampoDeBatalla.java
+++ src/gestorAplicacion/lugares/CampoDeBatalla.java
@@ -7,71 +7,20 @@ import java.io.*;
 
 public class CampoDeBatalla {
 	int tiempoEspera=1500;
+
+    int turno=0;
+    public void resetTurno(){
+        turno=1;
+    }
+    public int getturno(){
+        return turno;
+    }
+    public void seguirturno(){
+        turno++;
+    }
     public void ComenzarBatalla(Pokemon p1,Pokemon p2) {
         Scanner teclado = new Scanner(System.in);
-        if (p1.getVida()==0 || p2.getVida()==0){
-            System.out.println("Un pokemon no puede luchar por falta de vida");
-        }
-        else{
-            // COMIENZA BATALLA
 
-           tespe();
-            for(int i = 0; i <= 10; i = i + 1){
-                System.out.println();
-            }
-            int turno=1;
-
-            String titulo;
-            while (p2.getVida()>0 && p1.getVida()>0) {
-
-                for (int i = 0; i <= 10; i = i + 1) {
-                    System.out.println();
-                }
-                titulo="Turno :" + turno + "\n" + p1.pokvid() + " \t\t\tvs\t\t\t" + p2.pokvid() + "\n"+ "()\t\t\t\t\t\t\t\t\t\t\t\t() ";
-                System.out.println(titulo+"\n Lanzar:");
-                ArrayList<Habilidad> Habilidades = p1.getHabilidades();
-                for (int i = 0; i < Habilidades.size(); i++) {
-                    Habilidad element = Habilidades.get(i);
-                    System.out.println(String.valueOf(i + 1) + ") " + element);
-                }
-                int hab = Integer.parseInt(teclado.nextLine()) - 1;
-                while (hab + 1 > p1.getNumeroHabiidades()) {
-                    System.out.println("No disponible, digite otro");
-                    hab = Integer.parseInt(teclado.nextLine()) - 1;
-
-                }
-
-                p1.lanzarHabilidad(hab, p2);
-                tespe();
-
-                for (int i = 0; i <= 10; i = i + 1) {
-                    System.out.println();
-                }
-
-                turno++;
-                if (verificarvidas(p1,p2)!=""){
-                    System.out.println(verificarvidas(p1,p2));
-                    tespe();
-                    break;
-                }
-                turno++;
-                titulo="Turno :" + turno + "\n" + p1.pokvid() + " \t\t\tvs\t\t\t" + p2.pokvid() + "\n"+ "()\t\t\t\t\t\t\t\t\t\t\t\t() ";
-                System.out.println(titulo);
-                tespe();
-                System.out.println("El bot lanzar�");
-                ArrayList<Habilidad> Habilidadesb = p2.getHabilidades();
-
-
-                p2.lanzarHabilidad((int) (Math.random() * p2.getNumeroHabiidades() - 1), p1);
-
-                tespe();
-                if (verificarvidas(p1,p2)!=""){
-                    System.out.println(verificarvidas(p1,p2));
-                    tespe();
-                    break;
-                }
-            }
-        }
     }
     public void tespe(){
         try {
--- src/uiMain/POOkemon.java
+++ src/uiMain/POOkemon.java
@@ -129,11 +129,73 @@ public class POOkemon {
             int opcion= Integer.parseInt(teclado.nextLine());
             switch (opcion){
                 case 1:
+					CampoDeBatalla cb1 = new CampoDeBatalla();
+
+
                 	System.out.println(jugador.getGimnasio().getNombre());
                 	System.out.println(jugador.getGimnasio().getPOOKEMON());
                     CampoDeBatalla campo=new CampoDeBatalla();
-                    campo.ComenzarBatalla(jugador.getPokemonInicial(),pokRival);
-                    break;
+					Pokemon p1= jugador.getPokemonInicial();
+					Pokemon p2= pokRival;
+					if (p1.getVida()==0 || p2.getVida()==0){
+						System.out.println("Un pokemon no puede luchar por falta de vida");
+					}
+					else{
+						cb1.resetTurno();
+						campo.ComenzarBatalla(jugador.getPokemonInicial(),pokRival);
+						cb1.tespe();
+						for(int i = 0; i <= 10; i = i + 1){System.out.println();}
+						String titulo;
+						while (p2.getVida()>0 && p1.getVida()>0) {
+							for(int i = 0; i <= 10; i = i + 1){System.out.println();}
+							cb1.seguirturno();
+							titulo="Turno :" + cb1.getturno() + "\n" + p1.pokvid() + " \t\t\tvs\t\t\t" + p2.pokvid() + "\n"+ "()\t\t\t\t\t\t\t\t\t\t\t\t() ";
+							System.out.println(titulo+"\n Lanzar:");
+							ArrayList<Habilidad> Habilidades = p1.getHabilidades();
+							for (int i = 0; i < Habilidades.size(); i++) {
+								Habilidad element = Habilidades.get(i);
+								System.out.println(String.valueOf(i + 1) + ") " + element);
+							}
+							int hab = Integer.parseInt(teclado.nextLine()) - 1;
+							while (hab + 1 > p1.getNumeroHabiidades()) {
+								System.out.println("No disponible, digite otro");
+								hab = Integer.parseInt(teclado.nextLine()) - 1;
+
+							}
+
+							p1.lanzarHabilidad(hab, p2);
+							cb1.tespe();
+
+							for (int i = 0; i <= 10; i = i + 1) {
+								System.out.println();
+							}
+
+							if (cb1.verificarvidas(p1,p2)!=""){
+								System.out.println(cb1.verificarvidas(p1,p2));
+								cb1.tespe();
+								break;
+							}
+							cb1.seguirturno();
+							titulo="Turno :" + cb1.getturno() + "\n" + p1.pokvid() + " \t\t\tvs\t\t\t" + p2.pokvid() + "\n"+ "()\t\t\t\t\t\t\t\t\t\t\t\t() ";
+							System.out.println(titulo);
+							cb1.tespe();
+							System.out.println("El bot lanzar�");
+							ArrayList<Habilidad> Habilidadesb = p2.getHabilidades();
+
+
+							p2.lanzarHabilidad((int) (Math.random() * p2.getNumeroHabiidades() - 1), p1);
+
+							cb1.tespe();
+							if (cb1.verificarvidas(p1,p2)!=""){
+								System.out.println(cb1.verificarvidas(p1,p2));
+								cb1.tespe();
+								break;
+							}
+						}
+
+					}
+					cb1.seguirturno();
+					break;
                 case 2:
                     System.out.println(c1);
                     System.out.println(c1.CurarPokemon(jugador.getPokemonInicial()));
